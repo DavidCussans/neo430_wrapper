197f86a0b456f977212257b3a2c6a41b0025dfad
David Cussans <David.Cussans@bristol.ac.uk>
2020-01-21 15:15:18 +0000

    Removing obsolete files

--- firmware/cfg/neo430.tcl
+++
@@ -1,3 +0,0 @@
-set_property library neo430 [get_files neo430_package.vhd]
-set_property library neo430 [get_files neo430_application_image.vhd]
-set_property library neo430 [get_files neo430_bootloader_image.vhd]
